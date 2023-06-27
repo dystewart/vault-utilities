0191f3d7b74d175d621bde7d2a0e584e4613611c
dystewart <dystewar@redhat.com>
2023-06-27 19:35:13 -0400

    Cleanup script and move some files

--- vault-k8s-login/vault-login.go
+++ vault-k8s-login/vault-login.go
@@ -2,139 +2,47 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
+    "log"
     "os"
+    "path/filepath"
 
     "github.com/hashicorp/vault/api"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-    // Create the Vault client
-    client, err := createVaultClient()
+
+    // serviceAccount token path 
+    tokenPath := "/var/run/secrets/kubernetes.io/serviceaccount/token"
+
+    // Read the service account token from the file
+    token, err := ioutil.ReadFile(tokenPath)
     if err != nil {
-        fmt.Printf("Failed to create Vault client: %v\n", err)
-        return
+        log.Fatalf("Failed to read token file: %s", err)
     }
 
-    // Login with Kubernetes auth method
-    err = loginWithKubernetes(client)
-    if err != nil {
-        fmt.Printf("Failed to login with Kubernetes auth method: %v\n", err)
-        return
-    }
+    vault_addr := "http://nerc-vault.vault.svc:8200"
+    client, err := createVaultClient(vault_addr)
 
-    // Use the Vault client for further operations
-    // ...
+    fmt.Println("Token: %v", token)
+    fmt.Println("tokenPath: %v", tokenPath)
+    fmt.Println("vault addr: %v", client.Address())
+
 }
 
-func createVaultClient() (*api.Client, error) {
-    // Create a new Vault client
-    client, err := api.NewClient(api.DefaultConfig())
-    if err != nil {
-        return nil, err
+func createVaultClient(vault_addr string) (*api.Client, error) {
+    // Create a new Vault configuration with the specified address
+    config := &api.Config{
+    Address: vault_addr,
     }
 
-    // Set the Vault server address
-    client.SetAddress("http://your-vault-address:8200")
+    // Create a new Vault client using the configuration
+    client, err := api.NewClient(config)
+    if err != nil {
+        return nil, fmt.Errorf("Failed to create Vault client: %v", err)
+    }
 
     return client, nil
 }
 
-func loginWithKubernetes(client *api.Client) error {
-    // Authenticate with Vault using the Kubernetes auth method
-    authPath := "auth/kubernetes/login"
-    authData := map[string]interface{}{
-        "jwt":   getKubernetesToken(),
-        "role":  "your-kubernetes-role",
-        "mount": "your-kubernetes-auth-mount",
-    }
 
-    // Perform the login request
-    response, err := client.Logical().Write(authPath, authData)
-    if err != nil {
-        return err
-    }
-
-    // Check the login response and retrieve the Vault token if successful
-    if response.Auth == nil || response.Auth.ClientToken == "" {
-        return fmt.Errorf("Failed to authenticate with Kubernetes auth method")
-    }
-
-    // Set the obtained Vault token in the client for subsequent operations
-    client.SetToken(response.Auth.ClientToken)
-
-    return nil
-}
-
-func getKubernetesToken() string {
-    // Create a Kubernetes client
-    clientset, err := createKubernetesClient()
-    if err != nil {
-        fmt.Printf("Failed to create Kubernetes client: %v\n", err)
-        return ""
-    }
-
-    // Retrieve the service account token
-    token, err := getServiceAccountToken(clientset)
-    if err != nil {
-        fmt.Printf("Failed to retrieve service account token: %v\n", err)
-        return ""
-    }
-
-    return token
-}
-
-func createKubernetesClient() (*kubernetes.Clientset, error) {
-    // Get the path to the kubeconfig file
-    kubeconfigPath := os.Getenv("KUBECONFIG")
-    if kubeconfigPath == "" {
-        kubeconfigPath = clientcmd.RecommendedHomeFile
-    }
-
-    // Create a Kubernetes client from the kubeconfig file
-    config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-    if err != nil {
-        return nil, fmt.Errorf("failed to load kubeconfig: %v", err)
-    }
-
-    clientset, err := kubernetes.NewForConfig(config)
-    if err != nil {
-        return nil, fmt.Errorf("failed to create Kubernetes client: %v", err)
-    }
-
-    return clientset, nil
-}
-
-func getServiceAccountToken(clientset *kubernetes.Clientset) (string, error) {
-    // Get the default service account token in the current namespace
-    podName := os.Getenv("POD_NAME")
-    namespace := os.Getenv("POD_NAMESPACE")
-
-    pod, err := clientset.CoreV1().Pods(namespace).Get(podName, metav1.GetOptions{})
-    if err != nil {
-        return "", fmt.Errorf("failed to get pod: %v", err)
-    }
-
-    // Retrieve the service account token from the pod's mounted service account token volume
-    volumes := pod.Spec.Volumes
-    for _, volume := range volumes {
-        if volume.Secret != nil && volume.Secret.SecretName == "default-token" {
-            secret, err := clientset.CoreV1().Secrets(namespace).Get(volume.Secret.SecretName, metav1.GetOptions{})
-            if err != nil {
-                return "", fmt.Errorf("failed to get secret: %v", err)
-            }
-
-            tokenBytes, ok := secret.Data["token"]
-            if !ok {
-                return "", fmt.Errorf("service account token not found in secret")
-            }
-
-            token := string(tokenBytes)
-            return token, nil
-        }
-    }
-
-    return "", fmt.Errorf("service account token volume not found")
-}
